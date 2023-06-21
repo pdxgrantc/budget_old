e9c4793d8ec6ab5a8badf278d1bc8f688054594d
pdxgrantc <pdxgrantc@gmail.com>
2023-06-20 22:21:05 -0700

    remove password sign in

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -41,16 +41,20 @@ export default function SignIn() {
 
     return (
         <div className='h-full w-full'>
-            <div className='flex flex-col gap-5 rounded-md my-[3rem] mx-auto bg-black py-[2rem] px-[3.5rem] w-fit min-w-[45rem] text-small'>
+            <div className='flex flex-col gap-5 rounded-md my-[3rem] mx-auto bg-black py-[2rem] px-[3.5rem] w-fit text-small'>
                 <h1 className='text-lheader font-bold text-white'>Sign In</h1>
                 <div className='flex flex-col gap-8'>
+                    {/*
                     <div className='flex flex-col gap-[1.15rem]'>
                         <div className='flex flex-col gap-[.65rem]'>
                             <input className='text-black text-xsmall w-full py-2 px-3 rounded outline-none bg-input_grey' type='email' placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} />
                             <input className='text-black text-xsmall w-full py-2 px-3 rounded outline-none bg-input_grey' type='password' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
                         </div>
+                        <div>
+                        </div>
                         <button className='w-full py-2 px-3 rounded bg-button_grey text-text hover:bg-button_grey_hover transition' onClick={usernamePasswordSignIn}>Sign In</button>
                     </div>
+                    */}
                     <div className='font-semibold mx-auto'>
                         <GoogleButton />
                     </div>
