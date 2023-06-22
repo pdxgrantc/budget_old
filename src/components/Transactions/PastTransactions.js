e573cdfcd68bda4b672c30853432cdfc6ff4e94e
pdxgrantc <pdxgrantc@gmail.com>
2023-06-21 17:23:50 -0700

    work final

--- src/components/Transactions/PastTransactions.js
+++ src/components/Transactions/PastTransactions.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+// Firebase
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { auth } from '../../firebase'
 
 export default function PastTransactions() {
-    return (
-        <div>PastTransactions</div>
-    )
+    const [user] = useAuthState(auth)
+    const [transactions, setTransactions] = useState([])
+
+    // pull the last 50 transactions from the database at /users/{user.uid}/transactions
+    const useEffect(() => {
+        const unsubscribe = db
+            .collection('users')
+            .doc(user.uid)
+            .collection('transactions')
+            .orderBy('timestamp', 'desc')
+            .limit(50)
+            .onSnapshot((snapshot) => {
+                setTransactions(snapshot.docs.map((doc) => doc.data()))
+            })
+
+        return unsubscribe
+    }, [])
+    
+    if (user) {
+        return (
+            <div>PastTransactions</div>
+        )
+    }
 }
