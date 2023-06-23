506d666585a59a2bf81f1803272ddd4c024853de
pdxgrantc <pdxgrantc@gmail.com>
2023-06-23 11:43:47 -0700

    bug fix for income source delete

--- src/components/AccountSettings/IncomeSettings/IncomeSources.js
+++ src/components/AccountSettings/IncomeSettings/IncomeSources.js
@@ -1,23 +1,19 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
+import { doc, onSnapshot, updateDoc, getDoc } from 'firebase/firestore';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth, db } from '../../../firebase';
 
-// Firebase
-import { doc, getDocs, onSnapshot } from 'firebase/firestore'
-import { useAuthState } from 'react-firebase-hooks/auth'
-import { auth, db } from '../../../firebase'
-
-// Components
-import { FaRegTrashAlt as TrashIcon } from 'react-icons/fa'
+import { FaRegTrashAlt as TrashIcon } from 'react-icons/fa';
 
 export default function IncomeSources() {
-    const [user] = useAuthState(auth)
-    const [incomeSources, setIncomeSources] = useState([])
+    const [user] = useAuthState(auth);
+    const [incomeSources, setIncomeSources] = useState([]);
 
     useEffect(() => {
         const unsubscribe = onSnapshot(
             doc(db, 'users', user.uid),
             (doc) => {
-                setIncomeSources(doc.data().incomeSources
-                );
+                setIncomeSources(doc.data().incomeSources);
             }
         );
         return () => {
@@ -26,40 +22,43 @@ export default function IncomeSources() {
     }, [user, user.uid]);
 
     const handleDeleteIncomeSource = async (incomeSource) => {
-        const userRef = doc(db, 'users', user.uid);
+        const userDocRef = doc(db, 'users', user.uid);
 
-        // confirm deletion
+        // Confirm deletion
         const confirmDelete = window.confirm(`Are you sure you want to delete the income source "${incomeSource}"?`);
         if (!confirmDelete) {
             return;
         }
 
-        // remove income source from user's incomeSources array without using a transaction
-        const docCache = await getDocs(userRef);
-        const incomeSources = docCache.data().incomeSources;
-        const newIncomeSources = incomeSources.filter((incomeSourceItem) => incomeSourceItem !== incomeSource);
-        await userRef.update({ incomeSources: newIncomeSources });
+        // Remove income source from user's incomeSources array
+        const docSnapshot = await getDoc(userDocRef);
+        if (docSnapshot.exists()) {
+            const data = docSnapshot.data();
+            const incomeSourcesArray = data.incomeSources || [];
+            const newIncomeSources = incomeSourcesArray.filter((incomeSourceItem) => incomeSourceItem !== incomeSource);
+
+            await updateDoc(userDocRef, { incomeSources: newIncomeSources });
+        }
     };
 
-
     return (
-        <div className='flex flex-col gap-3'>
+        <div className="flex flex-col gap-3">
             <h2 className="text-sheader font-semibold">Current Income Sources</h2>
-            <div className='ml-3'>
-                {incomeSources !== null ? (
+            <div className="ml-3">
+                {incomeSources.length > 0 ? (
                     incomeSources.map((incomeSource, index) => (
-                        <div key={incomeSource} className='flex items-center gap-2'>
+                        <div key={incomeSource} className="flex items-center gap-2">
                             <p>{index + 1}.</p>
                             <p>{incomeSource}</p>
                             <button onClick={() => handleDeleteIncomeSource(incomeSource)} className=" ">
-                                <TrashIcon className='p-1 rounded-full h-[80%] w-full hover:text-black hover:bg-gray-200' />
+                                <TrashIcon className="p-1 rounded-full h-[80%] w-full hover:text-black hover:bg-gray-200" />
                             </button>
                         </div>
                     ))
                 ) : (
-                    <></>
+                    <p>No income sources found.</p>
                 )}
             </div>
         </div>
-    )
+    );
 }
