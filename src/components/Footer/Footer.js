c89f649b4674235eb92b64fb6b2c5cbd2edd433a
pdxgrantc <pdxgrantc@gmail.com>
2023-06-24 15:58:55 -0700

    footer bg

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
     return (
         <div>
             <div className='h-[4rem]'></div>
-            <div className='h-[50px] flex items-center justify-center'>
+            <div className='h-[50px] flex items-center justify-center bg-black py-[2.5rem] px-[3rem] rounded-main_box w-fit mx-auto'>
                 <h3 className='text-center text-small'>Grant Conklin | {year}</h3>
             </div>
             <div className='h-[4rem]'></div>
