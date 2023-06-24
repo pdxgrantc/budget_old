4a1ef0987c93f5731629bd1eaaa920be201444ca
pdxgrantc <pdxgrantc@gmail.com>
2023-06-24 16:03:33 -0700

    removed vestigial code

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Transactions from "./components/Transactions/Transactions";
 import Income from "./components/Income/Income";
-import BudgetSettings from "./components/BudgetSettings/BudgetSettings";
+//import BudgetSettings from "./components/BudgetSettings/BudgetSettings";
 import AccountSettings from "./components/AccountSettings/AccountSettings";
 import FourOFour from "./components/FourOFour/FourOFour";
 
@@ -17,7 +17,7 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/transactions" element={<Transactions />} />
         <Route path="/income" element={<Income />} />
-        <Route path="/budget-settings" element={<BudgetSettings />} />
+        {/*<Route path="/budget-settings" element={<BudgetSettings />} /> */}
         <Route path="/account-settings" element={<AccountSettings />} />
         <Route path="*" element={<FourOFour />} />
         <Route path='/404' element={<FourOFour />} />
