7aa69b34e333db69553d6c9658b520adb18649f8
pdxgrantc <47283493+pdxgrantc@users.noreply.github.com>
2023-07-03 12:15:33 -0700

    mobile footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
 
     return (
         <div>
-            <div className='h-[4rem]'></div>
+            <div className='on_desktop:h-[4rem] on_mobile:h-[2rem]'></div>
             <div className='h-[50px] flex items-center justify-center bg-black py-[2.5rem] px-[3rem] rounded-main_box w-fit mx-auto'>
                 <h3 className='text-center text-small'>Grant Conklin | {year}</h3>
             </div>
